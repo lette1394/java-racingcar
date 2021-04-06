d8ff5d431eb49feea7bb9895a96df0c3b46fa56d
lette1394 <lette1394@gmail.com>
2021-04-06 22:30:33 +0900

    refactor(racinggame): Inject MovementPolicy from outside

--- src/main/java/step1/RacingGame.java
+++ src/main/java/step1/RacingGame.java
@@ -1,24 +1,28 @@
 package step1;
 
-import java.util.Arrays;
+import static step1.Contracts.requires;
+
 import lombok.Builder;
 
-@Builder
 public class RacingGame {
   private final StringBuilder sb = new StringBuilder();
   private final int cars;
   private final int tries;
+  private final MovementPolicy movementPolicy;
+
+  @Builder
+  public RacingGame(int cars, int tries, MovementPolicy movementPolicy) {
+    requires(cars > 0, "cars > 0");
+    requires(tries > 0, "tries > 0");
+    requires(movementPolicy != null, "movementPolicy != null");
+
+    this.cars = cars;
+    this.tries = tries;
+    this.movementPolicy = movementPolicy;
+  }
 
   public void run() {
-    MovementPolicy policy = new PredefinedMovementPolicy(Arrays.asList(
-      Movement.FORWARD, Movement.FORWARD, Movement.FORWARD,
-      Movement.FORWARD, Movement.STAY, Movement.FORWARD,
-      Movement.FORWARD, Movement.FORWARD, Movement.FORWARD,
-      Movement.FORWARD, Movement.FORWARD, Movement.FORWARD,
-      Movement.STAY, Movement.FORWARD, Movement.FORWARD
-    ));
-
-    final Round round = new Round(policy);
+    final Round round = new Round(movementPolicy);
     for (int i = 0; i < tries; i++) {
       round.run();
       sb.append(round.print());
--- src/test/java/RacingGameTest.java
+++ src/test/java/RacingGameTest.java
@@ -1,14 +1,27 @@
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+import step1.Movement;
+import step1.MovementPolicy;
+import step1.PredefinedMovementPolicy;
 import step1.RacingGame;
 
 public class RacingGameTest {
   @Test
   void test1() {
+    final MovementPolicy movementPolicy = new PredefinedMovementPolicy(Arrays.asList(
+      Movement.FORWARD, Movement.FORWARD, Movement.FORWARD,
+      Movement.FORWARD, Movement.STAY, Movement.FORWARD,
+      Movement.FORWARD, Movement.FORWARD, Movement.FORWARD,
+      Movement.FORWARD, Movement.FORWARD, Movement.FORWARD,
+      Movement.STAY, Movement.FORWARD, Movement.FORWARD
+    ));
+
     final RacingGame racingGame = RacingGame.builder()
       .cars(3)
       .tries(5)
+      .movementPolicy(movementPolicy)
       .build();
 
     final String expected = ""
