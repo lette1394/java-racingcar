392bdd6ad92541201c8c5789f0bd6a923c4f8e43
lette1394 <lette1394@gmail.com>
2021-04-09 22:55:35 +0900

    refactor: Add BaseStreamm

---
+++ src/main/java/racing/BaseStream.java
@@ -0,0 +1,13 @@
+package racing;
+
+import java.util.stream.Stream;
+import lombok.experimental.Delegate;
+
+public abstract class BaseStream<T> implements Stream<T> {
+  @Delegate
+  private final Stream<T> delegate;
+
+  protected BaseStream(Stream<T> delegate) {
+    this.delegate = delegate;
+  }
+}
