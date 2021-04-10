4401e341290907a44494b9f27ae451e8aeb9d15a
lette1394 <lette1394@gmail.com>
2021-04-11 00:59:07 +0900

    refactor: StringPrinter

--- src/main/java/racing/view/StringPrinter.java
+++ src/main/java/racing/view/StringPrinter.java
@@ -1,11 +1,11 @@
 package racing.view;
 
 import java.util.stream.Collectors;
+import racing.domain.Car;
 import racing.domain.Cars;
 import racing.runner.GameResult;
 
 public class StringPrinter implements Printer {
-
   @Override
   public String print(GameResult gameResult) {
     return gameResult.carsStream()
@@ -13,16 +13,18 @@ public class StringPrinter implements Printer {
       .collect(Collectors.joining("\n\n"));
   }
 
-  public static String print(Cars cars) {
-    return cars.map(car -> {
-        final StringBuilder sb = new StringBuilder(256);
-        sb.append(car.name());
-        sb.append(" : ");
-        for (int i = 0; i < car.location(); i++) {
-          sb.append("-");
-        }
-        return sb.toString();
-      })
+  private static String print(Cars cars) {
+    return cars.map(StringPrinter::print)
       .collect(Collectors.joining("\n"));
+  }
+
+  private static String print(Car car) {
+    final StringBuilder sb = new StringBuilder(256);
+    sb.append(car.name());
+    sb.append(" : ");
+    for (int i = 0; i < car.location(); i++) {
+      sb.append("-");
+    }
+    return sb.toString();
   }
 }
