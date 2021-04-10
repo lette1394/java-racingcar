eeb457d9fd136609d1540630bd542acd3bdcf819
lette1394 <lette1394@gmail.com>
2021-04-11 00:56:05 +0900

    refactor: Use names() method

--- src/main/java/racing/view/StringWinnerPrinter.java
+++ src/main/java/racing/view/StringWinnerPrinter.java
@@ -1,15 +1,13 @@
 package racing.view;
 
 import java.util.stream.Collectors;
-import racing.domain.Car;
+import racing.domain.Names;
 import racing.runner.GameResult;
 
 public class StringWinnerPrinter implements Printer {
   @Override
   public String print(GameResult gameResult) {
-    final String winners = gameResult.winners()
-      .map(Car::name)
-      .collect(Collectors.joining(", "));
-    return winners + "가 최종 우승했습니다.";
+    final Names winnerNames = gameResult.winners().names();
+    return winnerNames.collect(Collectors.joining(", ")) + "가 최종 우승했습니다.";
   }
 }
