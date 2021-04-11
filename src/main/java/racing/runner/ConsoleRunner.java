2f300b2b9f30279451832aa008ee089ad3e7e7ce
lette1394 <lette1394@gmail.com>
2021-04-11 22:47:56 +0900

    refactor: rename class

--- src/main/java/racing/runner/ConsoleRunner.java
+++ src/main/java/racing/runner/ConsoleRunner.java
@@ -10,7 +10,7 @@ import racing.view.Printer;
 import racing.view.StringPrinter;
 import racing.view.StringWinnerPrinter;
 
-public class ConsoleMain {
+public class ConsoleRunner {
   public static void main(String[] args) {
     System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
     final Scanner scanner = new Scanner(System.in);
