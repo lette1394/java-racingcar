a8f7b26cfaa677084a208eeedf37b4801c6d6749
lette1394 <lette1394@gmail.com>
2021-04-11 22:33:26 +0900

    feat: Add main class

---
+++ src/main/java/racing/runner/ConsoleMain.java
@@ -0,0 +1,48 @@
+package racing.runner;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
+import racing.domain.Car;
+import racing.domain.CarFactory;
+import racing.domain.Movement;
+import racing.domain.MovementPolicy;
+import racing.view.Printer;
+import racing.view.StringPrinter;
+import racing.view.StringWinnerPrinter;
+
+public class ConsoleMain {
+  public static void main(String[] args) {
+    System.out.println("경주할 자동차 이름을 입력하세요(이름은 쉼표(,)를 기준으로 구분).");
+    final Scanner scanner = new Scanner(System.in);
+    // TODO: remove duplicate
+    final String[] names = scanner.nextLine().split(",");
+    System.out.println("시도할 회수는 몇회인가요?");
+    final int times = Integer.parseInt(scanner.nextLine());
+
+    final MovementPolicy random = () -> {
+      final long value = ThreadLocalRandom.current().nextLong(10);
+      if (value >= 4) {
+        return Movement.FORWARD;
+      }
+      return Movement.STAY;
+    };
+
+    final CarFactory carFactory = new CarFactory(random);
+    final Set<Car> cars = carFactory.create(Set.of(names));
+    final Game game = Game.builder()
+      .cars(cars)
+      .tries(times)
+      .build();
+
+    System.out.println();
+    System.out.println("실행 결과");
+    final GameResult gameResult = game.run();
+    final Printer print1 = new StringPrinter(Arrays.asList(names));
+    System.out.println(print1.print(gameResult));
+
+    final Printer print2 = new StringWinnerPrinter(Arrays.asList(names));
+    System.out.println(print2.print(gameResult));
+  }
+}
